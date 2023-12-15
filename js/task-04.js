293c9a044a39adae000013f727d379bafdb99f2c
oopshelga <134417737+oopshelga@users.noreply.github.com>
2023-12-15 16:04:38 +0200

    finished fifth

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,22 @@
 
+
+const registerForm = document.querySelector(".login-form");
+
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  
+  if (email === "" || password === "") {
+      alert('All form fields must be filled in');
+      return;
+  }
+
+//   console.log(`Email: ${login.value}, Password: ${password.value}`);
+  form.reset();
+}
+
+
